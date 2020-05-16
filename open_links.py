4801549092cb151f794bd541398470a4f2fcc0d5
Asadullah Hussain <asadxflow@gmail.com>
2020-05-16 04:11:50 +0200

    Adding simple script

--- open_links.py
+++ open_links.py
@@ -3,11 +3,9 @@ import sys
 import webbrowser
 
 try:
-    browser_path = 'C:/Program Files/Opera/launcher.exe'
-    # browser_path = "\"C:\Program Files\Opera\launcher.exe\""
-    #webbrowser.register('opera', None, webbrowser.BackgroundBrowser(browser_path))
     links = open ("link.txt", "r")
-    #browser = webbrowser.get("C:/Program Files/Opera/launcher.exe %s")
+    browser = webbrowser.get("C:/Program Files/Opera/launcher.exe %s")
+    #browser = webbrowser.get('opera')
     for line in links:
         browser.open(line,new=2,)
     links.close()
